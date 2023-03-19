cae88242eb104c8265bafe56a8419cb3f8a91961
anjumpathan <anjumpathan966@gmail.com>
2023-03-19 09:09:32 +0530

    Js assignment

---
+++ Calculater/cal.js
@@ -0,0 +1,34 @@
+function cal(val1, val2) {
+  let result = val1 + val2;
+  return result;
+}
+let add = cal(10, 20);
+console.log(add);
+
+function cal1(val1, val2) {
+  let sub1 = val1 - val2;
+  return sub1;
+}
+let sub = cal1(10, 20);
+console.log(sub);
+
+function cal2(val1, val2) {
+  let mult = val1 * val2;
+  return mult;
+}
+let mul = cal2(10, 40);
+console.log(mul);
+
+function cal3(val1, val2) {
+  let div = val1 / val2;
+  return div;
+}
+let division = cal3(10, 40);
+console.log(division);
+
+function cal4(val1, val2) {
+  let mod = val1 % val2;
+  return mod;
+}
+let m = cal4(10, 40);
+console.log(m);
